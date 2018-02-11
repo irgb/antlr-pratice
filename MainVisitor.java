036327df31be534e8b79715dc9eec51f07b676ae
Tao Shibo <taoshibo@baidu.com>
2018-02-11 11:38:02 +0800

    mv Main.java MainVisitor.java

--- MainVisitor.java
+++ MainVisitor.java
@@ -1,4 +1,29 @@
-public class CalculatorVisitorImpl extends CalculatorBaseVisitor<Integer> {
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class MainVisitor {
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length>0 ) inputFile = args[0];
+        InputStream is = System.in;
+        if (inputFile!=null ) is = new FileInputStream(inputFile);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+
+        CalculatorLexer lexer = new CalculatorLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        //System.out.println(tokens.getText());
+        CalculatorParser parser = new CalculatorParser(tokens);
+        ParseTree tree = parser.expr(); // parse
+
+        CalculatorVisitorImpl eval = new CalculatorVisitorImpl();
+        System.out.println(eval.visit(tree));
+    }
+}
+
+class CalculatorVisitorImpl extends CalculatorBaseVisitor<Integer> {
     @Override
     public Integer visitMulDiv(CalculatorParser.MulDivContext ctx) {
         System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
